35a29271177dad1e752ebfbbe5bb74c5e2148bdb
arcanelemon <63074135+arcanelemon@users.noreply.github.com>
2021-04-24 16:25:57 -0500

    CB-0003: Outlined Manual Functions. Implemented Depth Counter

--- project_nachos/GameManager.gd
+++ project_nachos/GameManager.gd
@@ -6,27 +6,54 @@ extends Node
 # var b = "text"
 
 export var total_time = 30
-onready var minute_multiplier= 60
-onready var depth_percentage = 0
 
+var current_time = 0
 
+var minute_multiplier= 60
+
+var depth_percentage = 0
+
+var leaks_total = 15 
+
+var leaks_repaired = 0 
+
+var pipes_total = 8 
+
+var pipes_repaired = 0 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	convert_time_to_minutes()
 	start_depth_timer()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	increment_depth_percentage()
+	pass
+
 
 func start_depth_timer():
+	$Timer.connect("timeout", self, "increment_depth_percentage")
 	$Timer.start()
 	
+
+func convert_time_to_minutes():
+	total_time *= 60
+
 func increment_depth_percentage():
-	depth_percentage = total_time * minute_multiplier / $Timer.time_left
-	print(depth_percentage)
+		current_time += 1
+		depth_percentage = float (current_time) / total_time
+		print("Depth percentage: " + depth_percentage)
 
 func get_depth_precentage():
 	return depth_percentage
+
+func get_leaks_remaining():
+	return leaks_total - leaks_repaired
+	
+func get_pipes_remaining():
+	return pipes_total - pipes_repaired
+
+func end_game():
+	pass
 
