510e6b83eb0c79f101bd5438f6cd709c1a877fdf
Kaleb Ashmore <kaleb.ashmore@gmail.com>
2021-04-26 17:09:53 -0500

    Added controls menu to title screen

--- project_nachos/Scenes/title_screen.gd
+++ project_nachos/Scenes/title_screen.gd
@@ -3,9 +3,15 @@ extends Control
 func _ready():
 	for button in $menu/button_container/buttons.get_children():
 		button.connect("pressed", self, "_on_button_pressed", [button.scene_to_load])
+	$controls_menu/back.connect("pressed", self, "_back_func")
 
 func _on_button_pressed(scene_to_load):
 	if scene_to_load == "EXIT_GAME":
 		get_tree().quit()
+	elif scene_to_load == "CONTROLS":
+		$controls_menu.visible = true
 	else:
 		get_tree().change_scene(scene_to_load)
+
+func _back_func() -> void:
+	$controls_menu.visible = false
