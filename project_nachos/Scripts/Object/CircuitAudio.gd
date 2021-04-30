e7b43aed87d73ee42c1bb1a2a52b5d1e98e0a946
Seth Barger <bargerseth@gmail.com>
2021-04-30 11:10:10 -0500

    v1.0.1 w key bug fix

--- project_nachos/Scripts/Object/CircuitAudio.gd
+++ project_nachos/Scripts/Object/CircuitAudio.gd
@@ -8,15 +8,16 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AudioStreamPlayer3D.stream.set_loop(false)
-	$Timer.set_wait_time(2)
+	$Timer.set_wait_time(1.5)
 	$Timer.connect("timeout", self, "spark")
-	yield(get_tree().create_timer(rand_range(.5, 5)), "timeout")
+	yield(get_tree().create_timer(rand_range(.2, .7)), "timeout")
 	$Timer.start()
 	$AudioStreamPlayer3D.play()
 
 func spark():
-	$AudioStreamPlayer3D.set_pitch_scale(rand_range(.5, 1.5))
+	$AudioStreamPlayer3D.set_pitch_scale(rand_range(1.5, 3.0))
 	$AudioStreamPlayer3D.play()
+	$Timer.set_wait_time(rand_range(.2, .7))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
